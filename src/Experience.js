4e2f8bbfc4ac9359b4c63d3639654482f0c61d09
Tomas Okal <tomas.okal@gmail.com>
2023-02-11 23:10:32 -0600

    Initial work on instancing.

--- src/Experience.js
+++ src/Experience.js
@@ -3,9 +3,9 @@ import { useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import * as THREE from 'three'
 
-import Model  from './Model'
 import useIcon from './stores/useIcon'
 import Lights from './Lights'
+import Boxes from './Boxes'
 
 export default function Experience()
 {
@@ -13,22 +13,12 @@ export default function Experience()
 
   const clicked = useIcon((state) => state.iconRotate)
 
-  useFrame((state, delta) => {
-    ref.current.rotation.y = THREE.MathUtils.lerp(
-      ref.current.rotation.y, 
-      clicked ? ref.current.rotation.y += delta / 10 : ref.current.rotation.y += 0, 
-      10
-    )
-  })
-
   return <>   
 
     <OrbitControls />
     <Lights />
 
-        <group ref={ref}>
-            <Model />
-        </group>
+    <Boxes i={350} j={350} />
           
   </>
 }

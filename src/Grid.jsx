f2f722a637807f1bf748abda1234587d6b1baf35
Tomas Okal <tomas.okal@gmail.com>
2023-05-07 22:26:47 -0500

    Update Grid component.

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -4,7 +4,6 @@ export default function Grid({ rows, columns })
 {
 
     const totalTiles = rows * columns
-    console.log(getNeighboringCells(7, 7, 1, 0))
 
     const tileSize = 2.25
 
@@ -16,13 +15,13 @@ export default function Grid({ rows, columns })
         const x = i % columns * tileSize
         const y = Math.floor(i / columns) * tileSize
 
-        tiles.push(<Tile key={i} positionX={x} positionZ={y} />)
+        tiles.push(
+            <Tile key={i} positionX={x} positionZ={y} />
+        )
 
     }
 
     return <>
-
-        <gridHelper />
 
         <group>
             {tiles}
@@ -32,23 +31,3 @@ export default function Grid({ rows, columns })
 
 
 }
-
-function getNeighboringCells(m, n, m_index, n_index) {
-
-    const neighbors = []
-
-    // Iterate over neighboring rows
-    for (let i = Math.max(0, m_index - 1); i <= Math.min(m - 1, m_index + 1); i++) {
-        // Iterate over neighboring columns
-        for (let j = Math.max(0, n_index - 1); j <= Math.min(n - 1, n_index + 1); j++) {
-            // Skip the current cell
-            if (i === m_index && j === n_index) {
-                continue
-            }
-            neighbors.push([i, j])
-        }
-    }
-
-    return neighbors
-}
-

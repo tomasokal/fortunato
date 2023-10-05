51889080a2497026cf9da52d2404203322a7bdb6
Tomas Okal <tomas.okal@gmail.com>
2023-10-04 22:07:33 -0500

    Add basic skeleton for checking tile and game state as well as opening and closing dialog.

--- src/DialogRefactor.jsx
+++ src/DialogRefactor.jsx
@@ -1,29 +1,50 @@
 import dialogNodes from './dialog/dialogNodes.json'
 import dialog from './dialog/dialog.json'
 
+import useGame from './stores/useGame'
+import { useEffect, useState } from 'react'
+
 export default function DialogRefactor()
 {
 
-    const currentDialogue = dialog[dialogNodes['gameStateClueTwo']]
+    const tile = useGame((state) => state.tile)
+
+    const foundClueOne = useGame((state) => state.foundClueOne)
+
+    const setFoundClueOne = useGame((state) => state.setFoundClueOne)
+
+    const [ showDialog, setShowDialog ] = useState(false)
+
+    const [ currentDialogue, setCurrentDialogue ] = useState({})
 
     function handleEvent(event)
     {
         event.preventDefault()
-
+        setShowDialog(false)
+        setCurrentDialogue({})
     }
 
-    // Subscribe to game state tile
-    // When on game state tile, run handleEvent using the event
-    // This should pass text, button text, and set that game state to yes
+    useEffect(()=> {
+        
+        // Check if tile is tileDeadBook and whether clue has been found
+        // If not, set clue found to true
+        if (tile === 'tileDeadBook' && foundClueOne === false)
+        {   
+            setCurrentDialogue(dialog[dialogNodes['gameStateClueOne']])
+            setShowDialog(true)
+            setFoundClueOne(true)
+        }
+
+    }, [tile])
     
     return <>
 
-        <dialog className="dialog" open={true}>
+        {showDialog && <dialog className="dialog" open={showDialog}>
             <form onSubmit={handleEvent}>
                 <p>{currentDialogue.text}</p>
                 <button type='submit'>{currentDialogue.button.text}</button>
             </form>
-        </dialog>
+        </dialog>}
 
     </>
 

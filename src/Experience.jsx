e452a1ce0b52f28087374de334ac21714eedf25b
Tomas Okal <tomas.okal@gmail.com>
2023-10-15 15:11:30 -0500

    Add performance check.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react'
 import { DirectionalLightHelper, SpotLightHelper } from 'three'
 import { Center, OrbitControls, useHelper } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
 
 import Grid from './Grid.jsx'
 import Lighting from './Lighting.jsx'
@@ -13,6 +14,8 @@ export default function Experience()
     const light = useRef()
     
     return <>
+
+        <Perf />
     
         <OrbitControls 
             makeDefault 
